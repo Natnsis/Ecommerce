520d2fb987616022db08405c9a86dc57b7929e64
Natnael <nsisay49@gmail.ocm>
2025-03-03 08:05:52 -0800

    removed the annoying route

--- Server/index.js
+++ Server/index.js
@@ -8,9 +8,7 @@ const path = require('path');
 const salt = 10;
 var port = 4000;
 
-//route imports
-const loginRouter = require('./routes/Login')
-const listOfCustomers = require('./routes/ListOfCustomer')
+
 
 
 const app = express();
@@ -62,14 +60,59 @@ const vendorStorage = multer.diskStorage({
     },
 });
 
-
+//image uploads
 const customerUpload = multer({ storage: customerStorage });
 const vendorUpload = multer({ storage: vendorStorage });
 const productUpload = multer({storage: productStorage});
 
-// login routes
-app.use("/login", loginRouter);
-app.use("/listOfCustomers", listOfCustomers);
+
+
+
+//login
+app.post('/login', (req, res) => {
+    const q = "SELECT * FROM admins WHERE username = (?)";
+    db.query(q, [req.body.username], (err, result) => {
+        if (err) return res.json({ Error: "error in login" });
+        if (result.length === 0) {
+            const query = "SELECT * FROM vendors WHERE username = (?)";
+            db.query(query, [req.body.username], (err, result) => {
+                if (err) return res.json({ Error: "error in login" });
+
+                if (result.length === 0) {
+                    const query = "SELECT * FROM customers WHERE username = (?)";
+                    db.query(query, [req.body.username], (err, result) => {
+                        if (err) return res.json({ Error: "error in login" });
+                        if (result.length === 0) return res.json({ Error: "username is incorrect" });
+                        bcrypt.compare(req.body.password, result[0].password, (err, isMatch) => { // customer success
+                            if (err) return res.json({ Error: "error in password" });
+                            if (!isMatch) return res.json({ Error: "password is incorrect" });
+                            req.session.user = result[0];
+                            req.session.username = req.body.username; // Store username in session
+                            res.json({ status: "login success", role: "customer" });
+                        });
+                    });
+                } else { 
+                    bcrypt.compare(req.body.password, result[0].password, (err, isMatch) => {
+                        if (err) return res.json({ Error: "error in password" });
+                        if (!isMatch) return res.json({ Error: "password is incorrect" });
+                        req.session.user = result[0];
+                        req.session.username = req.body.username; // Store username in session
+                        res.json({ status: "login success", role: "vendor" });
+                    });
+                }
+            });
+        } else {
+            if (req.body.password === result[0].password) {
+                req.session.user = result[0];
+                req.session.id = req.body.id;
+                req.session.username = req.body.username; // Store username in session
+                res.json({ status: "login success", role: "admin" });
+            } else {
+                res.json({ Error: "password is incorrect" });
+            }
+        }
+    });
+});
 
 
 //customer register
@@ -98,6 +141,16 @@ app.post('/register', customerUpload.single('image'), (req, res) => {
         });
     });
 });
+
+
+//get customers
+app.get('/listOfCustomers', (req, res)=>{
+    const q = 'SELECT * FROM customers';
+    db.query(q, (err, result)=>{
+        if(err) return res.json({Error:'error fetching customers'})
+        res.json(result);
+    })
+})
 
 //admin change password
 app.post('/change-password-admin', (req, res) => {
@@ -133,7 +186,7 @@ app.post('/addProducts', productUpload.single('image'), (req, res) => {
         req.body.price,
         req.file ? req.file.filename : null,
         req.body.category,
-        req.session.id
+        req.session.user.id 
     ];
 
     db.query(query, [values], (err, result) => {
