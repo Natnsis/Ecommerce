ac3753c863fac018c6778e69f9d18d19290b70b9
Nati <nsisay49@gmail.com>
2025-12-20 22:03:51 +0300

    Customer path on work

--- components/Cheader.tsx
+++ components/Cheader.tsx
@@ -10,13 +10,39 @@ import {
   SelectValue,
 } from "./ui/select";
 import { createClient } from "@/lib/supabase/client";
+import { useEffect, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { redirect } from "next/navigation";
 
-const Cheader = async () => {
-  const supabase = createClient();
-  const { data } = await supabase.auth.getClaims();
-  const user = data?.claims;
-  console.log("hehe")
-  console.log(user)
+const Cheader = () => {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const supabase = createClient();
+      const { data } = await supabase.auth.getClaims();
+      setUser(data?.claims ?? null);
+    };
+
+    loadUser();
+  }, []);
+
+  console.log(user);
+  if (!user) {
+    redirect('/login')
+  }
   return (
     <div className="px-5 py-3 flex justify-between">
       <h1 className="font-quater text-2xl">Gebeya</h1>
@@ -31,6 +57,7 @@ const Cheader = async () => {
           <ShoppingBag />
         </Button>
         <ModeToggle />
+
         <Select>
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Language" />
@@ -40,6 +67,22 @@ const Cheader = async () => {
             <SelectItem value="dark">Amharic</SelectItem>
           </SelectContent>
         </Select>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src={user.user_metadata?.picture} alt="profileImg" />
+              <AvatarFallback>{user.user_metadata?.full_name}</AvatarFallback>
+            </Avatar></DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{user.user_metadata?.email}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>{user.user_metadata?.full_name}</DropdownMenuItem>
+            <DropdownMenuItem>
+              <Button></Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
       </div>
     </div>
