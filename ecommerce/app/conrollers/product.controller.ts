a02ec140f18f7b4290fe045b108a8bcd7e7f43d5
Nati <nsisay49@gmail.com>
2026-01-27 14:26:16 +0300

    products image retrieval

--- ecommerce/app/conrollers/product.controller.ts
+++ ecommerce/app/conrollers/product.controller.ts
@@ -41,4 +41,68 @@ export const addProduct = async (data: productType) => {
   }
 };
 
+export const getProduct = async () => {
+  try {
+    const supabase = createClient();
+    const { data: products, error: productsError } = await supabase.from("products")
+      .select("*")
+    if (productsError) throw productsError;
+    return products
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
 
+export const getProductWithId = async (id: string) => {
+  try {
+    const supabase = createClient();
+    const { data: product, error: productError } = await supabase
+      .from("products")
+      .select("*")
+      .filter("id", "eq", id)
+      .maybeSingle();
+    if (productError) throw productError
+    return product
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export const deleteProduct = async (id: string) => {
+  try {
+    const supabase = createClient();
+    const { error: deleteError } = await supabase.from("products")
+      .delete()
+      .eq("id", id)
+    if (deleteError) throw deleteError
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateProduct = async (id: string, data: Partial<productType>) => {
+  try {
+    const supabase = createClient();
+    const { name, price, market, category, description } = data;
+
+    const { error } = await supabase
+      .from("products")
+      .update({
+        name,
+        price,
+        market,
+        category,
+        description,
+      })
+      .eq("id", id);
+
+    if (error) throw error;
+
+    return { message: "Product updated successfully" };
+  } catch (error: any) {
+    console.error("Update error:", error);
+    throw error;
+  }
+};
