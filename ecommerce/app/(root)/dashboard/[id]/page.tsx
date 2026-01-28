bcc0ce3237e679a6a58f013d75259fe4770c2b92
Nati <nsisay49@gmail.com>
2026-01-28 15:29:36 +0300

    cart ui

--- ecommerce/app/(root)/dashboard/[id]/page.tsx
+++ ecommerce/app/(root)/dashboard/[id]/page.tsx
@@ -5,11 +5,18 @@ import { ArrowLeftIcon, ShoppingBagOpenIcon, StarIcon } from "@phosphor-icons/re
 import { useParams, useRouter } from "next/navigation"
 import Image from 'next/image'
 import { Input } from "@/components/ui/input"
+import { useQuery } from "@tanstack/react-query"
+import { getProductWithId } from "@/app/conrollers/product.controller"
 
 const detail = () => {
   const router = useRouter()
   const { id } = useParams()
-  const product = products.find(p => p.id === Number(id));
+  const { data: product, isLoading } = useQuery({
+    queryKey: ['product', id],
+    queryFn: () => getProductWithId(id),
+    enabled: !!id
+  })
+
   return (
     <main className="h-screen p-5">
       <Button onClick={() => router.back()}>
@@ -18,8 +25,8 @@ const detail = () => {
       </Button>
       <section className="flex gap-5 h-[90vh] mt-10">
         <div className="bg-white p-10 w-1/2 h-[80vh] flex items-center justify-center">
-          <Image
-            src={product?.img!}
+          <img
+            src={product?.url}
             alt="product-img"
             width={400}
             height={500}
@@ -27,21 +34,21 @@ const detail = () => {
           />
         </div>
         <div className="p-10 w-1/2 flex flex-col justify-center gap-3">
-          <h1 className="text-5xl font-extrabold mb-2">{product?.title}</h1>
+          <h1 className="text-5xl font-extrabold mb-2">{product?.name}</h1>
           <div className="flex gap-2 items-center">
             <StarIcon color="#f6d32d" weight="fill" size={30} />
-            <p className="text-lg font-bold">{product?.rating}</p>
+            <p className="text-lg font-bold">5</p>
             <p className="text-gray-700 dark:text-gray-300">(based on {product?.reviews} reviews)</p>
           </div>
           <p className="text-gray-600 text-sm dark:text-gray-300">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            {product?.description}
           </p>
           <div>
             <p>${product?.price}/product</p>
           </div>
           <div className="flex gap-5">
-            <Input placeholder="Quantity" className="w-1/5" type="number" />
-            <Button className="w-4/5">Add to cart <ShoppingBagOpenIcon size={44} /></Button>
+            <Input placeholder="Quantity" className="w-2/5" type="number" />
+            <Button className="w-3/5">Add to cart <ShoppingBagOpenIcon size={44} /></Button>
           </div>
         </div>
       </section>
