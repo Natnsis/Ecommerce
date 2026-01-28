5f96432e3f1685f61222a83a2cdb21763811d4d8
Nati <nsisay49@gmail.com>
2026-01-28 15:39:57 +0300

    cart placeholders

--- ecommerce/app/(root)/dashboard/[id]/page.tsx
+++ ecommerce/app/(root)/dashboard/[id]/page.tsx
@@ -26,7 +26,7 @@ const detail = () => {
       <section className="flex gap-5 h-[90vh] mt-10">
         <div className="bg-white p-10 w-1/2 h-[80vh] flex items-center justify-center">
           <img
-            src={product?.url}
+            src={product?.url ? product?.url : "/product-placeholder.png"}
             alt="product-img"
             width={400}
             height={500}
@@ -34,17 +34,19 @@ const detail = () => {
           />
         </div>
         <div className="p-10 w-1/2 flex flex-col justify-center gap-3">
-          <h1 className="text-5xl font-extrabold mb-2">{product?.name}</h1>
+          <h1 className="text-5xl font-extrabold mb-2">
+            {product?.name ? product?.name : "Product Name"}
+          </h1>
           <div className="flex gap-2 items-center">
             <StarIcon color="#f6d32d" weight="fill" size={30} />
             <p className="text-lg font-bold">5</p>
             <p className="text-gray-700 dark:text-gray-300">(based on {product?.reviews} reviews)</p>
           </div>
           <p className="text-gray-600 text-sm dark:text-gray-300">
-            {product?.description}
+            {product?.description ? product?.description : "the product description will be here-"}
           </p>
           <div>
-            <p>${product?.price}/product</p>
+            <p>${product?.price ? product?.price : "1234-"}/product</p>
           </div>
           <div className="flex gap-5">
             <Input placeholder="Quantity" className="w-2/5" type="number" />
