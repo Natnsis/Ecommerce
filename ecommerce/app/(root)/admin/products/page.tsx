b715cafe30c5da3d3c3a447d9bead6fd577b1640
Nati <nsisay49@gmail.com>
2026-01-30 11:27:07 +0300

    products button

--- ecommerce/app/(root)/admin/products/page.tsx
+++ ecommerce/app/(root)/admin/products/page.tsx
@@ -12,11 +12,12 @@ const Products = () => {
       <Sidebar pageName="products" />
       <section className="px-5 col-span-7 p-5">
         <h1 className="text-4xl">Products</h1>
-        <div className="md:flex-row justify-between flex-col gap-2">
+        <div className="flex md:flex-row justify-between flex-col gap-2">
           <p className="text-sm text-gray-700 mt-2 dark:text-gray-500">
             List of products within the platform
           </p>
-          <Button onClick={() => router.push("/admin/add-product")}>
+          <Button
+            onClick={() => router.push("/admin/add-product")}>
             <StackPlusIcon />
             <p>Add Product</p>
           </Button>
