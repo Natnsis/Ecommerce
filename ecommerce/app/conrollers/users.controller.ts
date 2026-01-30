05dd76fcb1d05feb08f70593777142da586ca87c
Nati <nsisay49@gmail.com>
2026-01-30 13:35:16 +0300

    customer counts

---
+++ ecommerce/app/conrollers/users.controller.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@/lib/supabase/client"
+
+const supabase = createClient()
+
+export const getCustomersCount = async () => {
+  try {
+    const role = 'customer'
+    const { count, error: countError } = await supabase.from('profiles')
+      .select("*", { count: 'exact', head: true })
+      .eq("role", role)
+    if (countError) throw countError
+    return count
+  } catch (error) {
+    throw error
+  }
+}
--- ecommerce/components/Counts.tsx
+++ ecommerce/components/Counts.tsx
@@ -1,7 +1,18 @@
+import { getCustomersCount } from "@/app/conrollers/users.controller";
 import { Card, CardContent } from "@/components/ui/card";
 import { HandbagSimpleIcon, InvoiceIcon, UserIcon } from "@phosphor-icons/react"
+import { useQuery } from "@tanstack/react-query";
 
 const Counts = () => {
+
+  const { data: userCount, error: countError } = useQuery({
+    queryKey: ['user-count'],
+    queryFn: getCustomersCount
+  });
+
+  if (countError) throw countError
+
+  console.log(userCount)
   return (
     <div className="flex-col flex gap-5 w-full mt-5 md:flex-row">
       <Card className="w-full">
@@ -24,7 +35,7 @@ const Counts = () => {
               <UserIcon size={32} color="white" weight="fill" />
             </div>
             <div>
-              <h1 className="text-2xl font-bold">21,376</h1>
+              <h1 className="text-2xl font-bold">{userCount}</h1>
               <p className="text-gray-700">Number Of Customers</p>
             </div>
           </div>
