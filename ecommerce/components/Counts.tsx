50e92a426582231c8ecbe3ec59f1af18b18de844
Nati <nsisay49@gmail.com>
2026-02-04 15:46:21 +0300

    all 3 counts

--- ecommerce/components/Counts.tsx
+++ ecommerce/components/Counts.tsx
@@ -1,18 +1,32 @@
+import { useUser } from "@/app/(root)/context/user";
+import { getProducts } from "@/app/conrollers/product.controller";
+import { fetchTransactionById } from "@/app/conrollers/transaction.controller";
 import { getCustomersCount } from "@/app/conrollers/users.controller";
 import { Card, CardContent } from "@/components/ui/card";
 import { HandbagSimpleIcon, InvoiceIcon, UserIcon } from "@phosphor-icons/react"
 import { useQuery } from "@tanstack/react-query";
 
 const Counts = () => {
-
+  const { data: user } = useUser();
   const { data: userCount, error: countError } = useQuery({
     queryKey: ['user-count'],
     queryFn: getCustomersCount
   });
-
   if (countError) throw countError
 
-  console.log(userCount)
+  const { data: orderCount, error: orderError } = useQuery({
+    queryKey: ['order-count', user?.id],
+    queryFn: () => fetchTransactionById(user?.id!),
+    enabled: !!user?.id
+  });
+
+  const { data: productCount, error: productError } = useQuery({
+    queryKey: ['product-count'],
+    queryFn: getProducts
+  })
+
+  console.log(productCount?.length);
+
   return (
     <div className="flex-col flex gap-5 w-full mt-5 md:flex-row">
       <Card className="w-full">
@@ -22,7 +36,7 @@ const Counts = () => {
               <HandbagSimpleIcon size={32} color="white" weight="fill" />
             </div>
             <div>
-              <h1 className="text-2xl font-bold">21,376</h1>
+              <h1 className="text-2xl font-bold">{orderCount?.length}</h1>
               <p className="text-gray-700">Total Order</p>
             </div>
           </div>
@@ -48,7 +62,7 @@ const Counts = () => {
               <InvoiceIcon size={32} color="white" weight="fill" />
             </div>
             <div>
-              <h1 className="text-2xl font-bold">21,376</h1>
+              <h1 className="text-2xl font-bold">{productCount?.length}</h1>
               <p className="text-gray-700">Number Of Products</p>
             </div>
           </div>
